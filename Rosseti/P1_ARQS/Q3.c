b7cfdf010c84667e5e09e6c9954c3e33ed996c21
Silvio-Darc <silviodarcangelo@id.uff.br>
2025-11-06 11:42:46 -0300

    q3

--- Rosseti/P1_ARQS/Q3.c
+++ Rosseti/P1_ARQS/Q3.c
@@ -3,31 +3,31 @@
 
 //Q3 - 2,0 PONTOS
 void imp_zz(TABB* a){
-  TLSE* l1 = TLSE_inicializa();
-    TLSE* l2 = TLSE_inicializa();
-    if(!a) return;
-    l1 = TLSE_insere(l1, a->info);
-    while(l1 || l2){
-        while(l1){
-            TABB* atual = TABB_busca(a, l1->info);
-            if(atual->esq) l2 = TLSE_insere(l2, atual->esq->info);
-            if(atual->dir) l2 = TLSE_insere(l2, atual->dir->info);
-            printf("%d ", l1->info);
-            TLSE* temp = l1;
-            l1 = l1->prox;
-            free(temp);
-        }
-        while(l2){
-            TABB* atual = TABB_busca(a, l2->info);
-            if(atual->dir) l1 = TLSE_insere(l1, atual->dir->info);
-            if(atual->esq) l1 = TLSE_insere(l1, atual->esq->info);
-            printf("%d ", l2->info);
-            TLSE* temp = l2;
-            l2 = l2->prox;
-            free(temp);
-        }
-    }
-    printf("\n");
+  TLSE* l1 = NULL;
+  TLSE* l2 = NULL;
+  if(!a) return;
+  l1 = TLSE_insere(l1, a->info);
+  while(l1 || l2){
+      while(l1){
+          TABB* atual = TABB_busca(l1->info, a);
+          if(atual->esq) l2 = TLSE_insere(l2, atual->esq->info);
+          if(atual->dir) l2 = TLSE_insere(l2, atual->dir->info);
+          printf("%d ", l1->info);
+          TLSE* temp = l1;
+          l1 = l1->prox;
+          free(temp);
+      }
+      while(l2){
+          TABB* atual = TABB_busca(l2->info, a);
+          if(atual->dir) l1 = TLSE_insere(l1, atual->dir->info);
+          if(atual->esq) l1 = TLSE_insere(l1, atual->esq->info);
+          printf("%d ", l2->info);
+          TLSE* temp = l2;
+          l2 = l2->prox;
+          free(temp);
+      }
+  }
+  printf("\n");
 }
 
 int main(void){
